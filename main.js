abc930580818d5ab0ac04278f0ccc53874419fb2
Ahmad Fay Baihaqi <ahmadfaybaihaqi@gmail.com>
2024-08-14 10:30:54 +0700

    create variable and operation function

--- main.js
+++ main.js
@@ -7,14 +7,31 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-  return a - b;
+  return a * b;
 }
 
 function divide(a, b) {
-  return a - b;
+  return a / b;
 }
 
-console.log(add(1, 1));
-console.log(subtract(1, 1));
-console.log(multiply(1, 1));
-console.log(divide(1, 1));
+let num1 = 3;
+let operator = "+";
+let num2 = 5;
+
+function operation(num1, operator, num2) {
+  const operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+  };
+
+  const calculate = operations[operator];
+  return calculate ? calculate(num1, num2) : "Invalid operator";
+}
+
+console.log(operation(7, "+", 5));
+// console.log(add(1, 1));
+// console.log(subtract(1, 1));
+// console.log(multiply(1, 1));
+// console.log(divide(1, 1));
